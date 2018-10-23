f59dbc2f39187108e58d90dda6793494b9123394
Marsel Mustafin <marsel.mustafin@flatstack.com>
2018-10-23 23:10:49 +0300

    Add doctors and institutions links first results

--- etc/fast_process.py
+++ etc/fast_process.py
@@ -3,5 +3,4 @@ import fileinput
 with fileinput.FileInput("proxies.txt", inplace=True, backup='.bak') as proxies:
     for line in proxies:
         well_formed_proxy = "http://" + line.split(" ")[0]
-        print(well_formed_proxy)
         line.replace(line, well_formed_proxy)
