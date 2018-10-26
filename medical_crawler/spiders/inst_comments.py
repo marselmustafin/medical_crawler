0f4f2ed6c302c88628c5d1d4d1cf772318f7113b
Marsel Mustafin <marsel.mustafin@flatstack.com>
2018-10-26 22:56:08 +0300

    Crawler refactoring

--- medical_crawler/spiders/inst_comments.py
+++ medical_crawler/spiders/inst_comments.py
@@ -12,9 +12,8 @@ class InstCommentsSpider(scrapy.Spider):
     base_url = "https://prodoctorov.ru"
 
     def start_requests(self):
-        for institution_reviews_url in self.institutions_reviews_urls():
-            request = scrapy.Request(
-                url=institution_reviews_url, callback=self.parse)
+        for url in self.institutions_reviews_urls():
+            request = scrapy.Request(url=url, callback=self.parse)
             request.meta["comments_class"] = "table.rates tr"
             yield request
 
@@ -24,8 +23,8 @@ class InstCommentsSpider(scrapy.Spider):
 
         url = response.url
 
-        for comment_division in response.css(response.meta["comments_class"]):
-            yield self.parse_comment(comment_division, url)
+        for comment_div in response.css(response.meta["comments_class"]):
+            yield self.parse_comment(comment_div, url)
 
         doctor_comments_link = response.css(
             ".fetch_lpu_doctors_rates::attr(data-url)").extract_first()
@@ -37,40 +36,40 @@ class InstCommentsSpider(scrapy.Spider):
 
             yield request
 
-    def parse_comment(self, comment_division, url):
-        id = self.comment_id(comment_division)
-        avg_rate = comment_division.css(".avg_rate::text").extract_first()
-        avg_text = comment_division.css(".avg_text::text").extract_first()
-        datetime = comment_division.css(".datetime::text").extract_first()
+    def parse_comment(self, comment_div, url):
+        id = self.comment_id(comment_div)
+        avg_rate = comment_div.css(".avg_rate::text").extract_first()
+        avg_text = comment_div.css(".avg_text::text").extract_first()
+        datetime = comment_div.css(".datetime::text").extract_first()
 
-        content = self.filtered_content(comment_division, "p.comment2")
+        content = self.filtered_content(comment_div, "p.comment2")
 
         if not content:
-            content = self.filtered_content(comment_division, "p.comment")
+            content = self.filtered_content(comment_div, "p.comment")
 
         pos_content = self.filtered_content(
-            comment_division, "p.comment_plus")
+            comment_div, "p.comment_plus")
 
         neg_content = self.filtered_content(
-            comment_division, "p.comment_minus")
+            comment_div, "p.comment_minus")
 
         institution_id = self.institution_id(url)
         institution_name = self.institution_name
         institution_city = self.institution_city
 
-        author_name = comment_division.css(
+        author_name = comment_div.css(
             "div[itemprop=author]::text").extract_first()
-        author_operator = comment_division.css(
+        author_operator = comment_div.css(
             ".mobile_operator_img::attr(alt)").extract_first()
 
-        moderator_reply_divisions = comment_division.css(".moder div")
+        moderator_reply_divs = comment_div.css(".moder div")
 
-        reply = self.moderator_reply(moderator_reply_divisions)
+        reply = self.moderator_reply(moderator_reply_divs)
 
-        reply_datetime = comment_division.css(
+        reply_datetime = comment_div.css(
             ".moder .datetime::text").extract_first()
 
-        doctor_name = comment_division.css(
+        doctor_name = comment_div.css(
             "div[style='float: right']::text").extract_first()
 
         doctor_name = self.filtered_doctor_name(doctor_name)
@@ -94,18 +93,18 @@ class InstCommentsSpider(scrapy.Spider):
             doctor_name=doctor_name
         )
 
-    def comment_id(self, comment_division):
-        id = comment_division.css(
+    def comment_id(self, comment_div):
+        id = comment_div.css(
             "div[data-rtype='simple']::attr(data)").extract_first()
 
         if id:
             return int(id)
 
-        return int(comment_division.css(
+        return int(comment_div.css(
             "div[data-rtype='detail']::attr(data)").extract_first())
 
-    def filtered_content(self, comment_division, content_type_class):
-        content = comment_division.css(content_type_class).extract_first()
+    def filtered_content(self, comment_div, content_type_class):
+        content = comment_div.css(content_type_class).extract_first()
 
         if content and remove_tags(content):
             return remove_tags(content)
@@ -114,9 +113,9 @@ class InstCommentsSpider(scrapy.Spider):
         if name:
             return " ".join(name.strip().split(" ")[2:])
 
-    def moderator_reply(self, moder_divisions):
-        if moder_divisions:
-            reply_strings = moder_divisions[-1].css("::text").extract()
+    def moderator_reply(self, moder_divs):
+        if moder_divs:
+            reply_strings = moder_divs[-1].css("::text").extract()
             return " ".join(reply_strings).strip()
 
     def institution_id(self, url):
