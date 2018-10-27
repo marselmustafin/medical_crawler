486d9122046d8d69191940c1e366917db5870f71
Marsel Mustafin <marsel.mustafin@flatstack.com>
2018-10-27 09:24:12 +0300

    Add proxies import

--- etc/bare_ip_process.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-
-with fileinput.FileInput("proxies.txt", inplace=True, backup='.bak') as proxies:
-    for line in proxies:
-        well_formed_proxy = "http://" + line.split(" ")[0]
-        line.replace(line, well_formed_proxy)
---
+++ etc/import_proxies.py
@@ -0,0 +1,12 @@
+import requests
+import re
+
+PROXIES_URL = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt"
+IP_REGEXP = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{2,5}\b"
+
+request = requests.get(PROXIES_URL)
+downloaded_proxies = re.findall(IP_REGEXP, request.text)
+
+with open("proxies.txt", "w") as proxies:
+    for proxy in downloaded_proxies:
+        proxies.write("http://" + proxy + "\n")
